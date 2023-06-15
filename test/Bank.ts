b02f2129d1063e2b1e688c7c4d3ffb1086283eae
westkite1201 <westkite1201@gmail.com>
2023-06-15 18:31:06 +0900

    feat  :test 케이스 추가

--- test/Bank.ts
+++ test/Bank.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { DBank } from '../typechain-types';
+
 let dbank: DBank;
 
 describe('DBank', () => {
@@ -18,95 +19,92 @@ describe('DBank', () => {
   it('should allow depositing funds', async () => {
     const depositAmount = ethers.utils.parseEther('1.0');
     const [user] = await ethers.getSigners();
-
-    const initialBalance = await ethers.provider.getBalance(user.address);
-    console.log(
-      'Initial user balance:',
-      ethers.utils.formatEther(initialBalance),
-    );
+    const initialBankBalance = await dbank
+      .connect(user)
+      .getBalance(user.getAddress());
 
     const deposit = await dbank.connect(user).deposit({ value: depositAmount });
-    const tx: any = await deposit.wait();
-    const depositor = tx.events[0].args[0];
-    const value = tx.events[0].args[1];
+    const finalBankBalance = await dbank
+      .connect(user)
+      .getBalance(user.getAddress());
 
-    console.log('value = ', value);
-    console.log('depositor = ', depositor);
-    // const balanceAfter: any = await dbank.getBalance(_depositor1);
+    const expectedBankBalance = initialBankBalance.add(depositAmount);
+
+    expect(finalBankBalance).to.equal(expectedBankBalance);
   });
 
-  //   it('should allow withdrawing funds', async () => {
-  //     const depositAmount = ethers.utils.parseEther('1.0');
-  //     const withdrawAmount = ethers.utils.parseEther('0.5');
-  //     const signer = await ethers.getSigners();
-  //     user = signer[0];
-  //     await dbank.connect(user).deposit({ value: depositAmount });
+  it('should allow withdrawing funds', async () => {
+    const depositAmount = ethers.utils.parseEther('1.0');
+    const withdrawAmount = ethers.utils.parseEther('0.5');
+    const signer = await ethers.getSigners();
+    user = signer[0];
+    await dbank.connect(user).deposit({ value: depositAmount });
 
-  //     await expect(
-  //       dbank.connect(user).withdraw(withdrawAmount),
-  //     ).to.changeEtherBalance(user, withdrawAmount);
-  //   });
+    await expect(
+      dbank.connect(user).withdraw(withdrawAmount),
+    ).to.changeEtherBalance(user, withdrawAmount);
+  });
 
-  //   it('should not allow depositing zero amount', async () => {
-  //     const signer = await ethers.getSigners();
-  //     user = signer[0];
-  //     await expect(dbank.connect(user).deposit({ value: 0 })).to.be.revertedWith(
-  //       'Deposit amount should be greater than zero.',
-  //     );
-  //   });
+  it('should not allow depositing zero amount', async () => {
+    const signer = await ethers.getSigners();
+    user = signer[0];
+    await expect(dbank.connect(user).deposit({ value: 0 })).to.be.revertedWith(
+      'Deposit amount should be greater than zero.',
+    );
+  });
 
-  //   it('should not allow withdrawing zero amount', async () => {
-  //     const signer = await ethers.getSigners();
-  //     user = signer[0];
-  //     await expect(dbank.connect(user).withdraw(0)).to.be.revertedWith(
-  //       'Withdrawal amount should be greater than zero.',
-  //     );
-  //   });
+  it('should not allow withdrawing zero amount', async () => {
+    const signer = await ethers.getSigners();
+    user = signer[0];
+    await expect(dbank.connect(user).withdraw(0)).to.be.revertedWith(
+      'Withdrawal amount should be greater than zero.',
+    );
+  });
 
-  //   it('should not allow withdrawing more than the account balance', async () => {
-  //     const signer = await ethers.getSigners();
-  //     user = signer[0];
-  //     const depositAmount = ethers.utils.parseEther('1.0');
-  //     const withdrawAmount = ethers.utils.parseEther('2.0');
+  it('should not allow withdrawing more than the account balance', async () => {
+    const signer = await ethers.getSigners();
+    user = signer[0];
+    const depositAmount = ethers.utils.parseEther('1.0');
+    const withdrawAmount = ethers.utils.parseEther('2.0');
 
-  //     await dbank.connect(user).deposit({ value: depositAmount });
+    await dbank.connect(user).deposit({ value: depositAmount });
 
-  //     await expect(
-  //       dbank.connect(user).withdraw(withdrawAmount),
-  //     ).to.be.revertedWith('Insufficient balance.');
-  //   });
+    await expect(
+      dbank.connect(user).withdraw(withdrawAmount),
+    ).to.be.revertedWith('Insufficient balance.');
+  });
 
-  //   it('should emit Deposit event when funds are deposited', async () => {
-  //     const signer = await ethers.getSigners();
-  //     user = signer[0];
-  //     const depositAmount = ethers.utils.parseEther('1.0');
+  it('should emit Deposit event when funds are deposited', async () => {
+    const signer = await ethers.getSigners();
+    user = signer[0];
+    const depositAmount = ethers.utils.parseEther('1.0');
 
-  //     await expect(dbank.connect(user).deposit({ value: depositAmount }))
-  //       .to.emit(dbank, 'Deposit')
-  //       .withArgs(user.address, depositAmount);
-  //   });
+    await expect(dbank.connect(user).deposit({ value: depositAmount }))
+      .to.emit(dbank, 'Deposit')
+      .withArgs(user.address, depositAmount);
+  });
 
-  //   it('should emit Withdrawal event when funds are withdrawn', async () => {
-  //     const signer = await ethers.getSigners();
-  //     user = signer[0];
-  //     const depositAmount = ethers.utils.parseEther('1.0');
-  //     const withdrawAmount = ethers.utils.parseEther('0.5');
+  it('should emit Withdrawal event when funds are withdrawn', async () => {
+    const signer = await ethers.getSigners();
+    user = signer[0];
+    const depositAmount = ethers.utils.parseEther('1.0');
+    const withdrawAmount = ethers.utils.parseEther('0.5');
 
-  //     await dbank.connect(user).deposit({ value: depositAmount });
+    await dbank.connect(user).deposit({ value: depositAmount });
 
-  //     await expect(dbank.connect(user).withdraw(withdrawAmount))
-  //       .to.emit(dbank, 'Withdrawal')
-  //       .withArgs(user.address, withdrawAmount);
-  //   });
+    await expect(dbank.connect(user).withdraw(withdrawAmount))
+      .to.emit(dbank, 'Withdrawal')
+      .withArgs(user.address, withdrawAmount);
+  });
 
-  //   it('should return the correct balance for an account', async () => {
-  //     const signer = await ethers.getSigners();
-  //     user = signer[0];
-  //     const depositAmount = ethers.utils.parseEther('1.0');
+  it('should return the correct balance for an account', async () => {
+    const signer = await ethers.getSigners();
+    user = signer[0];
+    const depositAmount = ethers.utils.parseEther('1.0');
 
-  //     await dbank.connect(user).deposit({ value: depositAmount });
+    await dbank.connect(user).deposit({ value: depositAmount });
 
-  //     const userBalance = await dbank.getBalance(user.address);
-  //     expect(userBalance).to.equal(depositAmount);
-  //   });
+    const userBalance = await dbank.getBalance(user.address);
+    expect(userBalance).to.equal(depositAmount);
+  });
 });
